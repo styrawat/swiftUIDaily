f8fe024bb1e2cbcd45641cbd336f23237125efbe
Satyam Rawat <satyamrawat636@gmail.com>
2026-04-22 01:35:05 +0530

    textfield

---
+++ addingText/addingText/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  addingText
+//
+//  Created by satyam kumar rawat on 22/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Satyam..")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ addingText/addingText/addingTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  addingTextApp.swift
+//  addingText
+//
+//  Created by satyam kumar rawat on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct addingTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
